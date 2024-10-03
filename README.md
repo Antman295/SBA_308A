41696bbe40b27137988186849ff733a3ac4d995e
Antman295 <antjosh14@gmail.com>
2024-10-03 14:46:46 -0400

    Update
    Now prints out location, state, current condition, the temperature and what it feels like. Just need to make it where it appears on the page

---
+++ README.md
@@ -0,0 +1,3 @@
+Weather API I used: https://www.weatherapi.com/
+Base to build weather app: https://www.geeksforgeeks.org/build-a-weather-app-in-html-css-javascript/
+
--- scripts/scripts.mjs
+++ scripts/scripts.mjs
@@ -1,20 +1,33 @@
 let form = document.getElementById('weatherLocation');
+let city = document.getElementById('location');
 
 
 // async function to collect data from the API
 async function getData(city) {
     const apiKey = '6273319fd5474074877181012240310';
 
+    // Try/Catch to catch error if something went wrong with the API
     try {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
-    const jsonData = await response.json();
     
+    // If/else to retrieve city from user if valid
     if (response.ok) {
+        const jsonData = await response.json();
+
+        const location = jsonData.location.name;
+        const region = jsonData.location.region;
+        const condition = jsonData.current.condition.text;
+        const temp = jsonData.current.temp_f;
+        const tempFeels = jsonData.current.feelslike_f;
+
+        console.log(location, region, condition, temp, tempFeels);
         console.log(jsonData);
     } else {
+        city.focus(); // Will focus on text field if city name is invalid
         alert(`${jsonData} is not a city. Please enter a valid one!`);
     }
     } catch (error) {
+        
         console.error('Error retreiving weather data: ', error)
     }
 }
