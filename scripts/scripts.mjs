4495ce8a94ecd2a5f0a7248eac7f1df2dced123a
Antman295 <antjosh14@gmail.com>
2024-10-03 14:24:31 -0400

    Update
    Created the README.md file and also got an API to get weather from a website. Plus, entering a city and getting the location works, just need to figure out how to print the weather.

--- scripts/scripts.mjs
+++ scripts/scripts.mjs
@@ -1,5 +1,27 @@
-async function getData() {
-    const response = await fetch("https://api.weather.gov");
+let form = document.getElementById('weatherLocation');
+
+
+// async function to collect data from the API
+async function getData(city) {
+    const apiKey = '6273319fd5474074877181012240310';
+
+    try {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
     const jsonData = await response.json();
-    console.log(jsonData);
+    
+    if (response.ok) {
+        console.log(jsonData);
+    } else {
+        alert(`${jsonData} is not a city. Please enter a valid one!`);
+    }
+    } catch (error) {
+        console.error('Error retreiving weather data: ', error)
+    }
 }
+
+// Event Listener to submit form and get location
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let city = document.getElementById('location').value;
+    getData(city);
+})
