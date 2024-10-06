d1cc5e499cde43e1424354a2fa5d009732107d6b
Antman295 <antjosh14@gmail.com>
2024-10-06 19:38:19 -0400

    Update
    Added one more comment and added one minor thing to the message that appears when the user hits submit

--- scripts/ui.mjs
+++ scripts/ui.mjs
@@ -2,13 +2,15 @@
 export function displayWeather(jsonData) {
     let message = document.getElementById('message');
 
+    // All the data that will be used from the API
     const location = jsonData.location.name;
     const state = jsonData.location.region;
     const condition = jsonData.current.condition.text;
     const temp = jsonData.current.temp_f;
     const tempFeels = jsonData.current.feelslike_f;
 
-    message.textContent = `In ${location}, ${state}, it's currently ${condition}, ${temp} degrees (in fahrenheit) but it feels like ${tempFeels}.` 
+    // Message that will appear after the user hits submit
+    message.textContent = `In ${location}, ${state}, it's currently ${condition}, ${temp} degrees (in fahrenheit) but it feels like ${tempFeels} degrees.` 
 }
 
 export function showError() {
